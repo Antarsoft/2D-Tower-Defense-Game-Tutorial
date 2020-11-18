7d816b2c2434b650c3ec5a0328d7ef5bbf480750
Ziad <ziad10a@live.com>
2020-11-18 21:08:44 +0400

    001 - Main Menu

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+   public void ChangeScene(string sceneName)
+   {
+       SceneManager.LoadScene(sceneName);
+   }
+}
