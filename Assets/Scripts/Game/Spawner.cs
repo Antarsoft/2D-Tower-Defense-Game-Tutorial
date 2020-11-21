5aa38ec92e555bd0844cb060001dd78bef22f94b
Ziad <ziad10a@live.com>
2020-11-21 19:55:26 +0400

    Spawn System Part 1

---
+++ Assets/Scripts/Game/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{    
+    //list of towers (prefabs) that will instatiate
+    public List<GameObject> towers;
+    //id of tower to spawn
+    int spawnID=-1;
+    
+
+    
+}
