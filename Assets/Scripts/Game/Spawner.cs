eb38cc0349f8c83a3fbb512f9344e67779a361dc
Ziad <ziad10a@live.com>
2020-12-19 23:43:41 +0400

    Unity 2D Tower Defense Tutorial 4 - Tower Spawn System Part 2

--- Assets/Scripts/Game/Spawner.cs
+++ Assets/Scripts/Game/Spawner.cs
@@ -1,14 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
-{    
+{
+    public SpriteRenderer testSprite;
+
     //list of towers (prefabs) that will instatiate
-    public List<GameObject> towers;
+    public List<GameObject> towersPrefabs;
+    //Transform of the spawning towers (Root Object)
+    public Transform spawnTowerRoot;
+    //list of towers (UI)
+    public List<Image> towersUI;
     //id of tower to spawn
     int spawnID=-1;
+    //SpawnPoints Tilemap
+    public Tilemap spawnTilemap;
     
+    void Update()
+    {
+        if(CanSpawn())
+            DetectSpawnPoint();
+    }
+
+    bool CanSpawn()
+    {
+        if (spawnID == -1)
+            return false;
+        else
+            return true;
+    }
+
+    void DetectSpawnPoint()
+    {
+        //Detect when mouse is clicked (first touch clicked)
+        if(Input.GetMouseButtonDown(0))
+        {
+            //get the world space poistion of the mouse
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //get the position of the cell in the tilemap
+            var cellPosDefault = spawnTilemap.WorldToCell(mousePos);
+            //get the center position of the cell
+            var cellPosCentered = spawnTilemap.GetCellCenterWorld(cellPosDefault);
+            //check if we can spawn in that cell (collider)
+            if(spawnTilemap.GetColliderType(cellPosDefault)== Tile.ColliderType.Sprite)
+            {
+                //Spawn the tower
+                SpawnTower(cellPosCentered);
+                //Disable the collider
+                spawnTilemap.SetColliderType(cellPosDefault,Tile.ColliderType.None);                
+            }                                  
+        }
+    }
+
+    void SpawnTower(Vector3 position)
+    {
+        GameObject tower = Instantiate(towersPrefabs[spawnID],spawnTowerRoot);
+        tower.transform.position = position;
+
+        DeselectTowers();
+    }
+
+    public void SelectTower(int id)
+    {
+        DeselectTowers();
+        //Set the spawnID
+        spawnID = id;
+        //Highlight the tower
+        towersUI[spawnID].color = Color.white;        
+    }
+
+    public void DeselectTowers()
+    {
+        spawnID = -1;
+        foreach(var t in towersUI)
+        {
+            t.color = new Color(0.5f, 0.5f, 0.5f);
+        }
+    }    
+
 
     
 }
