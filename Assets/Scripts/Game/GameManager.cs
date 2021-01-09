445eb5b2e732a229d0d11c43462d7725b272e639
ziad10a <ziad10a@live.com>
2021-01-09 15:14:43 +0400

    Health System

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    void Awake() { instance = this; }
+
+    void Start()
+    {
+        GetComponent<HealthSystem>().Init();
+    }    
+
+}
---
+++ Assets/Scripts/Game/HealthSystem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{    
+    //The UI text for the health count
+    public Text txt_healthCount;
+    //The default value of the health count (used for init)
+    public int defaultHealthCount;
+    //Current health count
+    public int healthCount;    
+
+
+    //Init the health system (reset the health count)
+    public void Init()
+    {
+        healthCount = defaultHealthCount;
+        txt_healthCount.text = healthCount.ToString();
+    }
+
+    //Lose health count
+    public void LoseHealth()
+    {
+        if (healthCount < 1)
+            return;
+
+        healthCount--;
+        txt_healthCount.text = healthCount.ToString();
+
+        CheckHealthCount();
+    }
+
+    //Check health count for losing
+    void CheckHealthCount()
+    {
+        if(healthCount<1)
+        {
+            Debug.Log("You lost");
+            //Call some reset values and stop the game from the manager
+        }
+    }
+}
